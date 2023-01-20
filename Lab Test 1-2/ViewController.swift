462ced5434eb21c684af57bd72aa681f1ee48b53
Elvin Ross Fabella <elvinross.fabella@gmail.com>
2023-01-20 17:14:51 -0500

    Initial Commit

---
+++ Lab Test 1-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab Test 1-2
+//
+//  Created by Elvin Ross Fabella on 2023-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
