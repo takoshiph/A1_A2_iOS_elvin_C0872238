f32ab9943d5a0566d00b840e78abbc0a6f85d78a
Elvin Ross Fabella <elvinross.fabella@gmail.com>
2023-01-21 00:29:25 -0500

    first commit

---
+++ Lab Test 1-2/Annotation.swift
@@ -0,0 +1,22 @@
+//
+//  LocalityAnnotation.swift
+//  Lab Test 1-2
+//
+//  Created by Elvin Ross Fabella on 2023-01-20.
+//
+
+import Foundation
+import MapKit
+
+
+class LocalityAnnotation: NSObject, MKAnnotation {
+    var title: String?
+    var subtitle: String?
+    var coordinate: CLLocationCoordinate2D
+    
+    init(title: String? = nil, subtitle: String? = nil, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.subtitle = subtitle
+        self.coordinate = coordinate
+    }
+}
